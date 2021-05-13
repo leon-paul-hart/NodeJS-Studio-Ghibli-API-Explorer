baa41139ea201b3f6ec295127a98876331e1546b
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-05-13 02:31:13 +0100

    Added placeholder code for HTTPS, refactored HTTP server setup as well.

--- NodeJS_Studio_Ghibli_API_Explorer/server.js
+++ NodeJS_Studio_Ghibli_API_Explorer/server.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const http = require('http');
-const port = process.env.PORT || 3030;
-
 const app = express();
+
+// HTTP server setup
+const http = require('http');
+const httpPort = process.env.PORT || 8080;
+const httpServer = http.createServer(app)
+httpServer.listen(httpPort);
+
+// HTTPS server setup
+//var fs = require('fs');
+//var https = require('https');
+//const httpsPort = process.env.PORT || 8443;
+//var privateKey = fs.readFileSync('certificates/key.pem', 'utf8');
+//var certificate = fs.readFileSync('certificates/cert.pem', 'utf8');
+//var credentials = { key: privateKey, cert: certificate };
+//var httpsServer = https.createServer(credentials, app);
+//httpsServer.listen(httpsPort);
 
 // Set static content folder
 app.use(express.static('public'));
@@ -10,5 +23,6 @@ app.use(express.static('public'));
 // Routes
 app.use('/', require('./routes/index'));
 
-// Set up HTTP server
-http.createServer(app).listen(port, () => console.log(`Express server now running at http://localhost:${port}`));
+// Console notifications
+console.log(`HTTP server listening at http://localhost:${httpPort}`);
+//console.log(`HTTPS server listening at http://localhost:${httpsPort}`);
