3ee824a90946da8efb5da3022b4a255d0dbb511e
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-05-13 02:15:19 +0100

    Created new router for Index.html

--- NodeJS_Studio_Ghibli_API_Explorer/server.js
+++ NodeJS_Studio_Ghibli_API_Explorer/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const path = require('path');
 const http = require('http');
 const port = process.env.PORT || 3030;
 
 const app = express();
 
-// Route to homepage
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 // Set static content folder
 app.use(express.static('public'));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 // Set up HTTP server
 http.createServer(app).listen(port, () => console.log(`Express server now running at http://localhost:${port}`));
