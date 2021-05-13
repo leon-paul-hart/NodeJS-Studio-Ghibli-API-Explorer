6a9b2a8952a13cda3f8a9adb514f8159442ff597
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-05-13 01:41:56 +0100

    Removed unused code

--- NodeJS_Studio_Ghibli_API_Explorer/public/js/app.js
+++ NodeJS_Studio_Ghibli_API_Explorer/public/js/app.js
@@ -1,16 +1,11 @@
 var app = function () {
 
-  var filmList = new FilmList("https://ghibliapi.herokuapp.com/films");
-  var filmListView = new FilmListView(document.querySelector("#films"));
-  var filmDetailsView = new FilmDetailsView(document.querySelector("#film-details"))
+    var filmList = new FilmList("https://ghibliapi.herokuapp.com/films");
+    var filmListView = new FilmListView(document.querySelector("#films"));
 
-  filmList.getData((films) => {
-    filmListView.render(films);
-    filmListView.selectElement.addEventListener("change", function () {
-      var foundFilm = films[this.value];
-      filmDetailsView.renvder(foundFilm);
+    filmList.getData((films) => {
+        filmListView.render(films);
     });
-  });
 
 }
 
--- NodeJS_Studio_Ghibli_API_Explorer/server.js
+++ NodeJS_Studio_Ghibli_API_Explorer/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3030;
 
 const app = express();
 
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 
 // Set static content folder
 app.use(express.static('public'));
+
 
 // Set up HTTP server
 http.createServer(app).listen(port, () => console.log(`Express server now running at http://localhost:${port}`));
