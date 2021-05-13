ff4000726cab79bc6b5cdc313850a2c3a9c074e4
Leon-Paul Hart <24394216+leon-paul-hart@users.noreply.github.com>
2021-05-13 02:35:52 +0100

    Added content type to router

--- NodeJS_Studio_Ghibli_API_Explorer/routes/index.js
+++ NodeJS_Studio_Ghibli_API_Explorer/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 // Route to index.html
 router.get('/', (req, res) => {
+    res.header('Content-type', 'text/html');
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
